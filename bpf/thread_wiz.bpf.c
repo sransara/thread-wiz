91a26bc7de107a99de73d77dd26e7f3dfd7e56e6
Samodya Abeysiriwardane <git@sransara.com>
2025-03-17 06:10:18 -0500

    support filtering by process id

--- bpf/thread_wiz.bpf.c
+++ bpf/thread_wiz.bpf.c
@@ -1,16 +1,33 @@
 #include "vmlinux.h"
 
+#include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+const volatile pid_t pid_for_observation = 0;
+const int max_task_traversal = 10;
+
+bool is_process_under_observation(struct task_struct *task) {
+  // BPF only allows bounded loops, so we limit the number of tasks to traverse
+  for (int i = 0; task != NULL && i < max_task_traversal; i++) {
+    pid_t this_pid = BPF_CORE_READ(task, tgid);
+    if (this_pid == pid_for_observation) {
+      return true;
+    }
+    task = BPF_CORE_READ(task, parent);
+  }
+  return false;
+}
+
 SEC("tp/sched/sched_switch")
-int
-handle_exec(struct trace_event_raw_sched_switch* ctx)
-{
-  struct task_struct* task = (struct task_struct*)bpf_get_current_task();
+int handle_exec(struct trace_event_raw_sched_switch *ctx) {
+  struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+  if (!is_process_under_observation(task)) {
+    return 0;
+  }
   bpf_printk("PID %d PPID: %d CTXPPID %d CTXNPID %d.\n",
-             task->pid,
-             task->parent->pid,
-             ctx->prev_pid,
-             ctx->next_pid);
+             BPF_CORE_READ(task, pid), BPF_CORE_READ(task, parent, pid),
+             ctx->prev_pid, ctx->next_pid);
   return 0;
 }
--- src/thread_wiz.cc
+++ src/thread_wiz.cc
@@ -1,15 +1,22 @@
 #include "thread_wiz.skel.h"
 #include <iostream>
+#include <string>
 
-int
-main()
-{
-  thread_wiz_bpf* skel = thread_wiz_bpf::open();
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <pid>\n";
+    return 1;
+  }
+  const int pid_for_observation = std::stoi(argv[1]);
+
+  thread_wiz_bpf *skel = thread_wiz_bpf::open();
   if (skel == nullptr) {
     std::cerr << "Failed to open and load BPF object\n";
     return 1;
   }
-  thread_wiz_bpf::load(skel);
+
+  skel->rodata->pid_for_observation = pid_for_observation;
+
   auto err = thread_wiz_bpf::load(skel);
   if (err != 0) {
     std::cerr << "Failed to load BPF object\n";
