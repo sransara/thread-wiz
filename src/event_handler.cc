f82ce055f82f620dcedc673a97d108bd07425709
Samodya Abeysiriwardane <git@sransara.com>
2025-03-18 07:18:10 -0500

    push all events to the websocket server

--- src/event_handler.cc
+++ src/event_handler.cc
@@ -1,33 +1,49 @@
 #include "event_handler.h"
 #include "thread_wiz.bpf.h"
+#include "web_child.h"
 #include <cstddef>
+#include <cstdio>
 #include <iostream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 int event_handler(void *ctx, void *data, size_t data_sz) {
   (void)ctx;
   (void)data_sz;
 
   const struct event *event = static_cast<const struct event *>(data);
+  json jsono;
   switch (event->type) {
   case THREAD_TRACE_NEW:
-    std::cout << "New thread: " << event->event.event_trace_new.comm
-              << " (pid: " << event->event.event_trace_new.pid << ") on CPU "
-              << event->event.event_trace_new.target_cpu << "\n";
+    jsono = {{"type", "THREAD_TRACE_NEW"},
+             {"comm", event->event.event_trace_new.comm},
+             {"pid", event->event.event_trace_new.pid},
+             {"target_cpu", event->event.event_trace_new.target_cpu}};
     break;
   case THREAD_TRACE_SWITCH:
-    std::cout << "Thread switch: " << event->event.event_trace_switch.prev_pid
-              << " (tgid: " << event->event.event_trace_switch.prev_tgid
-              << ") -> " << event->event.event_trace_switch.next_pid
-              << " (tgid: " << event->event.event_trace_switch.next_tgid
-              << ") on CPU " << event->event.event_trace_switch.cpu << "\n";
+    jsono = {
+        {"type", "THREAD_TRACE_SWITCH"},
+        {"timestamp_ns", event->event.event_trace_switch.timestamp_ns},
+        {"prev_state", event->event.event_trace_switch.prev_state},
+        {"cpu", event->event.event_trace_switch.cpu},
+        {"prev_pid", event->event.event_trace_switch.prev_pid},
+        {"prev_tgid", event->event.event_trace_switch.prev_tgid},
+        {"next_pid", event->event.event_trace_switch.next_pid},
+        {"next_tgid", event->event.event_trace_switch.next_tgid},
+        {"prev_is_observed", event->event.event_trace_switch.prev_is_observed},
+        {"next_is_observed", event->event.event_trace_switch.next_is_observed}};
     break;
   case THREAD_TRACE_TERMINATE:
-    std::cout << "Thread terminate: " << event->event.event_trace_terminate.pid
-              << "\n";
+    jsono = {{"type", "THREAD_TRACE_TERMINATE"},
+             {"pid", event->event.event_trace_terminate.pid}};
     break;
   default:
+    jsono = {{"type", "UNKNOWN"}, {"event_type", event->type}};
     std::cerr << "Unknown event type: " << event->type << "\n";
     break;
   }
+
+  write_to_web_process(jsono);
   return 0;
 }
